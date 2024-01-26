86be90017b7bc895f3b1ebd5aac450f9c3cc87d7
kyu <2066apple@naver.com>
2024-01-26 16:38:26 +0900

    feat: prisma db init

---
+++ prisma/migrations/20240126073711_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "links" TEXT
+);
