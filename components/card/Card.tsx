eff6c579d6ec7b6ebca0d3e93038c7078520e360
kyu <2066apple@naver.com>
2024-01-27 12:28:10 +0900

    fix: card hover

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -7,7 +7,7 @@ export const Card = (props: CardProps) => {
     const { title, content, onClick } = props
     return (
         <div
-            className="w-[350px] h-32 p-3 rounded flex flex-col bg-slate-100 text-black cursor-pointer"
+            className="w-[350px] h-32 p-3 rounded flex flex-col bg-slate-200 text-black cursor-pointer hover:bg-slate-300"
             onClick={onClick}
         >
             <div className="header w-full h-1/3 font-bold truncate">{title}</div>
